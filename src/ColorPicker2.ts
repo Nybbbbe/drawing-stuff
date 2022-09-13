9a4318e32a3a9b559128b2de76b4139f7800619b
Jan Nyberg <jan.nyberg@gmail.com>
2022-09-14 02:20:42 +0300

    Color picker 2 visualized

--- src/ColorPicker2.ts
+++ src/ColorPicker2.ts
@@ -6,7 +6,10 @@ class ColorPicker2 {
 
   // mouseEvents
   private isDragging: boolean = false;
-  private selectionPos: number = 0;
+  private selectionPos = {
+    x: 0,
+    y: 0
+  }
 
   constructor() {
     this.canvas = document.createElement('canvas');
@@ -19,24 +22,48 @@ class ColorPicker2 {
     this.resize();
     window.addEventListener('resize', this.resize);
     this.handleMouseEvents();
-    // requestAnimationFrame(this.draw);
+    requestAnimationFrame(this.draw);
   }
 
   private resize = () => {
-    this.canvas.width = 128;
-    this.canvas.height = 128;
+    this.canvas.width = 500;
+    this.canvas.height = 500;
   }
 
-  private drawGradiant = (i: number, c1: string, c2: string) => {
-    const gh = this.canvas.height / 6
+  private drawBackground = () => {
+    const gh = this.canvas.height
     const gw = this.canvas.width
-    const y = gh * i;
-    let grd = this.ctx.createLinearGradient(0, y, 0, y + gh);
+    const grdB = this.ctx.createLinearGradient(0, 0, 0, gh);
+    const grdW = this.ctx.createLinearGradient(gw, 0, 0, 0);
+
+    this.ctx.globalCompositeOperation = 'destination-over';
+
+    grdB.addColorStop(0, "rgba(0, 0, 0, 0)");
+    grdB.addColorStop(1, "rgba(0, 0, 0, 1)");
+    grdW.addColorStop(0, "rgba(255, 255, 255, 0)");
+    grdW.addColorStop(1, "rgba(255, 255, 255, 1)");
+    this.ctx.beginPath();
+
+    this.ctx.fillStyle = grdB;
+    this.ctx.fillRect(0, 0, gw, gh);
+    this.ctx.fillStyle = grdW;
+    this.ctx.fillRect(0, 0, gw, gh);
+    this.ctx.fillStyle = colorPickerState.currentChosenColor.colorString;
+    this.ctx.fillRect(0, 0, gw, gh);
+    this.ctx.closePath();
+    this.ctx.stroke();
+  }
+
+  private drawGradiantBW = (c1: string, c2: string) => {
+    const gh = this.canvas.height
+    const gw = this.canvas.width
+    let grd = this.ctx.createLinearGradient(gw, 0, 0, gh);
     this.ctx.beginPath();
     grd.addColorStop(0, c1);
     grd.addColorStop(1, c2);
+    this.ctx.globalAlpha = 0.5;
     this.ctx.fillStyle = grd;
-    this.ctx.fillRect(0, y, gw, gh);
+    this.ctx.fillRect(0, 0, gw, gh);
     this.ctx.closePath();
     this.ctx.stroke();
   }
@@ -45,64 +72,68 @@ class ColorPicker2 {
     this.ctx.beginPath();
     this.ctx.fillStyle = 'rgba(0, 0, 0, 0)';
     this.ctx.lineWidth = 5;
-    this.ctx.strokeStyle = "black";  
-    this.ctx.rect(0, this.selectionPos - 20, this.canvas.width, 10);
+    this.ctx.strokeStyle = "black";
+    this.ctx.arc(this.selectionPos.x, this.selectionPos.y, 10, 0, 2 * Math.PI);
     this.ctx.closePath();
     this.ctx.stroke();
   }
 
-  private findInterval = (): number[] => {
-    const intervals = [
-      0,
-      (this.canvas.height / 6),
-      (this.canvas.height / 6) * 2,
-      (this.canvas.height / 6) * 3,
-      (this.canvas.height / 6) * 4,
-      (this.canvas.height / 6) * 5,
-      (this.canvas.height / 6) * 6,
-      this.canvas.height
-    ]
-    for (let i = 0; i < intervals.length - 1; i++) {
-      const minVal = intervals[i];
-      const maxVal = intervals[i + 1];
-      if (this.selectionPos >= minVal && this.selectionPos <= maxVal) {
-        return [i, minVal];
-      }
-    }
-    return [-1, -1]
-  }
+  // private findInterval = (): number[] => {
+  //   // const intervals = [
+  //   //   0,
+  //   //   (this.canvas.height / 6),
+  //   //   (this.canvas.height / 6) * 2,
+  //   //   (this.canvas.height / 6) * 3,
+  //   //   (this.canvas.height / 6) * 4,
+  //   //   (this.canvas.height / 6) * 5,
+  //   //   (this.canvas.height / 6) * 6,
+  //   //   this.canvas.height
+  //   // ]
+  //   // for (let i = 0; i < intervals.length - 1; i++) {
+  //   //   const minVal = intervals[i];
+  //   //   const maxVal = intervals[i + 1];
+  //   //   if (this.selectionPos >= minVal && this.selectionPos <= maxVal) {
+  //   //     return [i, minVal];
+  //   //   }
+  //   // }
+  //   // return [-1, -1]
+  // }
 
   private getChosenColor = () => {
-    const [i, minVal] = this.findInterval();
-    const colorNum = ((this.selectionPos - minVal) / (this.canvas.height / 6)) * 255;
-    console.log(colorNum)
-    switch (i) {
-      case 0:
-        return `rgb(255, ${colorNum}, 0)`;
-      case 1:
-        return `rgb(${255 - colorNum}, 255, 0)`;
-      case 2:
-        return `rgb(0, 255, ${colorNum})`;
-      case 3:
-        return `rgb(0, ${255 - colorNum}, 255)`;
-      case 4:
-        return `rgb(${colorNum}, 0, 255)`;
-      case 5:
-        return `rgb(255, 0, ${255 - colorNum})`;
-    }
+    // const [i, minVal] = this.findInterval();
+    // const colorNum = ((this.selectionPos - minVal) / (this.canvas.height / 6)) * 255;
+    // console.log(colorNum)
+    // switch (i) {
+    //   case 0:
+    //     return `rgb(255, ${colorNum}, 0)`;
+    //   case 1:
+    //     return `rgb(${255 - colorNum}, 255, 0)`;
+    //   case 2:
+    //     return `rgb(0, 255, ${colorNum})`;
+    //   case 3:
+    //     return `rgb(0, ${255 - colorNum}, 255)`;
+    //   case 4:
+    //     return `rgb(${colorNum}, 0, 255)`;
+    //   case 5:
+    //     return `rgb(255, 0, ${255 - colorNum})`;
+    // }
   }
 
   private draw = () => {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    
+    if (colorPickerState.currentChosenColor.colorString !== '') {
+      this.drawSelector();
+      this.drawBackground();
+    }
+    
+    // this.drawGradiant(1, "rgb(255, 255, 0)", "rgb(0, 255, 0)");
+    // this.drawGradiant(2, "rgb(0, 255, 0)", "rgb(0, 255, 255)");
+    // this.drawGradiant(3, "rgb(0, 255, 255)", "rgb(0, 0, 255)");
+    // this.drawGradiant(4, "rgb(0, 0, 255)", "rgb(255, 0, 255)");
+    // this.drawGradiant(5, "rgb(255, 0, 255)", "rgb(255, 0, 0)");
 
-    this.drawGradiant(0, "rgb(255, 0, 0)", "rgb(255, 255, 0)");
-    this.drawGradiant(1, "rgb(255, 255, 0)", "rgb(0, 255, 0)");
-    this.drawGradiant(2, "rgb(0, 255, 0)", "rgb(0, 255, 255)");
-    this.drawGradiant(3, "rgb(0, 255, 255)", "rgb(0, 0, 255)");
-    this.drawGradiant(4, "rgb(0, 0, 255)", "rgb(255, 0, 255)");
-    this.drawGradiant(5, "rgb(255, 0, 255)", "rgb(255, 0, 0)");
-
-    this.drawSelector();
+    
 
     requestAnimationFrame(this.draw)
   }
@@ -115,21 +146,30 @@ class ColorPicker2 {
     window.addEventListener('mousemove', this.onPointerMove);
   }
 
-  private getCanvasMousePos = (e: MouseEvent): number => {
+  private getCanvasMousePosY = (e: MouseEvent): number => {
     const rect = this.canvas.getBoundingClientRect();
-    return e.clientY - rect.top;
+    return e.clientY - rect.top - 10;
+  }
+
+  private getCanvasMousePosX = (e: MouseEvent): number => {
+    const rect = this.canvas.getBoundingClientRect();
+    return e.clientX - rect.left - 10;
   }
 
   private onPointerDown = (e: MouseEvent) => {
     this.isDragging = true;
-    this.selectionPos = Math.min(Math.max(this.getCanvasMousePos(e), 0), this.canvas.height);
-    colorPickerState.setNewColor(this.getChosenColor()!);
+    this.selectionPos = {
+      x: Math.min(Math.max(this.getCanvasMousePosX(e), 0), this.canvas.width),
+      y: Math.min(Math.max(this.getCanvasMousePosY(e), 0), this.canvas.height)
+    }
   }
 
   private onPointerMove = (e: MouseEvent) => {
     if (this.isDragging) {
-      this.selectionPos = Math.min(Math.max(this.getCanvasMousePos(e), 0), this.canvas.height);
-      colorPickerState.setNewColor(this.getChosenColor()!);
+      this.selectionPos = {
+        x: Math.min(Math.max(this.getCanvasMousePosX(e), 0), this.canvas.width),
+        y: Math.min(Math.max(this.getCanvasMousePosY(e), 0), this.canvas.height)
+      }
     }
     
   }
@@ -139,5 +179,5 @@ class ColorPicker2 {
   }
 }
 
-const colorPicker = new ColorPicker2();
-export default colorPicker;
+const colorPicker2 = new ColorPicker2();
+export default colorPicker2;
--- src/PickerComponent.tsx
+++ src/PickerComponent.tsx
@@ -2,9 +2,11 @@ import { makeAutoObservable } from "mobx";
 import { observer } from "mobx-react-lite";
 import { useEffect, useRef } from "react";
 import colorPicker, { ChosenColor } from "./ColorPicker";
+import colorPicker2 from "./ColorPicker2";
 import './PickerComponent.scss';
 
 colorPicker;
+colorPicker2;
 
 class ColorPickerState {
   currentChosenColor: ChosenColor = {
@@ -26,21 +28,24 @@ export const colorPickerState = new ColorPickerState();
 
 const PickerComponent = (observer(() => {
   const canvasRef = useRef(null);
-    useEffect(() => {
-      colorPicker.updateRefs(canvasRef.current! as HTMLCanvasElement);
-    }, [])
+  const canvasRef2 = useRef(null);
 
-    return (
-      <div id="pickerContainer">
-        <canvas id="picker-canvas" ref={canvasRef}></canvas>
-        <div>
-          <h2>Current color:</h2>
-          <p>{colorPickerState.currentChosenColor.colorString}</p>
-          <div className="show-color-div" style={{backgroundColor: colorPickerState.currentChosenColor.colorString}}></div>
-        </div>
+  useEffect(() => {
+    colorPicker.updateRefs(canvasRef.current! as HTMLCanvasElement);
+    colorPicker2.updateRefs(canvasRef2.current! as HTMLCanvasElement);
+  }, [])
+
+  return (
+    <div id="pickerContainer">
+      <canvas id="picker-canvas" ref={canvasRef}></canvas>
+      <div>
+        <h2>Current color:</h2>
+        <p>{colorPickerState.currentChosenColor.colorString}</p>
+        <div className="show-color-div" style={{backgroundColor: colorPickerState.currentChosenColor.colorString}}></div>
+        <canvas id="picker-canvas-2" ref={canvasRef2}></canvas>
       </div>
-    )
-  })
-)
+    </div>
+  ) 
+}))
 
 export default PickerComponent;
